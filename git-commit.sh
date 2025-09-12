ad500f6b9d07c46f0e460513734063144f61917b
Eduard P. <144683920+bonobix@users.noreply.github.com>
2025-09-12 12:56:48 +0200

    Update git-commit.sh

--- git-commit.sh
+++ git-commit.sh
@@ -1,6 +1,6 @@
 git init
 git add .
 git commit -m "Aggiungo i file misteriosi al grimorio"
-git remote add origin git@github.com:80N080/bash-scripts.git
+git remote add origin git@github.com:bonobix/bash-scripts.git
 git pull origin main --allow-unrelated-histories  # se serve unire storie diverse
 git push -u origin main
