84c98cd5b86e627ca444f048b0471cc80cea66bd
Eduard P. <144683920+bonobix@users.noreply.github.com>
2025-10-10 14:00:07 +0200

    Update and rename dir-size.sh to process-n-storage/dir-size.sh

--- process-n-storage/dir-size.sh
+++ process-n-storage/dir-size.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+"""
+
+Scegli dalla lista e ricevi output della dimensione della directory scelta.
+
+"""
+
 echo "Seleziona una directory:"
 select dir in */ ; do
   if [[ -n "$dir" ]]; then
