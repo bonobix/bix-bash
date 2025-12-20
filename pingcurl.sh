7597c9f04d27afa75e90b140b94990ab8fccaddc
Your Name <you@example.com>
2025-12-20 15:13:33 +0100

    aggiungo pingcurl.sh

---
+++ pingcurl.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+: <<'COMMENT'
+
+esegue curl con output in stile ping 
+
+COMMENT
+
+
+
+while true; do
+    echo -n "$(date '+%H:%M:%S') - "
+    curl -k -s https://192.168.1.120:8123/bo
+    sleep 1
+done
